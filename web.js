66551251d76cee61ce5223f7c6f06982a4637c55
ABradshaw <abradshaw@sumall.com>
2015-03-09 18:48:50 -0400

    First pass at modifying a hipchat bot

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var ack = require('ac-koa').require('hipchat');
 var pkg = require('./package.json');
 var app = ack(pkg);
 
+var requests = [];
+
 var addon = app.addon()
   .hipchat()
   .allowRoom(true)
@@ -11,8 +13,30 @@ if (process.env.DEV_KEY) {
   addon.key(process.env.DEV_KEY);
 }
  
-addon.webhook('room_message', /^\/hello$/, function *() {
-  yield this.roomClient.sendNotification('Hi, ' + this.sender.name + '!');
+addon.webhook('room_message', /^\/add\s\w+/i, function *() {
+    var request = this.sender.name;
+    yield this.roomClient.sendNotification('Adding ' + request + ' to the list!');
+    requests.push(request);
+});
+
+addon.webhook('room_message', /^\/list$/, function *() {
+    yield this.roomClient.sendNotification('The List: \n' + requests.join(","));
+});
+
+addon.webhook('room_message', /^\/scrap$/, function *() {
+    requests = [];
+    yield this.roomClient.sendNotification('The list has been scrapped.');
+});
+
+addon.webhook('room_message', /^\/remove$/, function *() {
+    var request = this.sender.name;
+    var index = requests.indexOf(request);
+    if (index > -1) {
+        requests.splice(index, 1);
+        yield this.roomClient.sendNotification(request + ' has been removed from the list.');
+    } else {
+        yield this.roomclient.sendNotification(request + ' is not on the list so it hasn\'t been removed.');
+    }
 });
  
 app.listen();
